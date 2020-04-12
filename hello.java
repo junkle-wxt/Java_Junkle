1bfd5d31f639bd57a6ffbfe24db86532bbb1a262
junkle <wxt_junkle@qq.com>
2020-04-12 20:09:34 +0800

    diyigecaozuode

---
+++ hello.java
@@ -0,0 +1,8 @@
+@Test
+    public void test(){
+        Ab a = ()->{
+            System.out.println("woshinidie");
+			System.out.println("woshiOther");
+			System.out.println("woshiOther");
+            return 5;
+        };
